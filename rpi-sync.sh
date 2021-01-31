54fe807bd7df6e188ef6a4eed4f3f0349427cc87
Robert Krahn <robert.krahn@gmail.com>
2021-02-01 00:14:35 +0100

    cleanup

--- rpi-sync.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-HOST=rpi
-
-ssh $HOST "bash --login -c 'mkdir -p projects/fritz-homeautomation-rust/'"
-rsync --exclude=target -avzP --delete ./ $HOST:projects/fritz-homeautomation-rust/
-
-ssh rpi "bash --login -c 'cd projects/fritz-homeautomation-rust/; cargo build --release; systemctl restart homeautomation-server.service; journalctl -u homeautomation-server.service -f'"
-# ssh $HOST "bash --login -c 'cd projects/fritz-homeautomation-rust/; cargo build --release'"
